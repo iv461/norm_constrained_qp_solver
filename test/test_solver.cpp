38072b1a74e79bdc5e2ed919dda1eefd1dd6e683
Ivo Ivanov <ivo.ivanov@hs-augsburg.de>
2024-12-27 09:29:00 +0100

    Added test solver file

---
+++ test/test_solver.cpp
@@ -0,0 +1,17 @@
+#include <norm_constrained_qp_solver.hpp>
+
+#include <fmt/core.h>
+#include <fmt/iostream.h>
+
+using namespace ncs;
+
+int main() {
+
+    using Scalar = double; 
+    using Mat = Eigen::Matrix3<Scalar>;
+    using Vec = Eigen::Vector3<Scalar>;
+
+    
+
+    return 0;
+}
